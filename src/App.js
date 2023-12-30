d3f6a0dd74df7821d6acfa3838be1e304cafd70a
Amaan Bilwar <bilwarad@mail.uc.edu>
2023-12-30 00:40:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Error from './Error';
+
+import { useRandomReveal } from 'react-random-reveal'
+
+const AddSuspenseComponent = () => {
+  const characters = useRandomReveal({
+    characterSet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*',
+    revealEasing: 'linear',
+    isPlaying: true,
+    duration: 2,
+    characters: 'Amaan Bilwar',
+  })
+
+  return (
+    <div className='random-text'>
+      {characters}
+      </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <AddSuspenseComponent className='random-text'/>
+      <div className="main-title">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Website!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <ul className='list-items'>
+          <li>I'm a Computer Engineering student at <a href="https://www.uc.edu/" target ='_blank' >Univerity of Cincinnati</a>
+          </li>
+          <li>I'm a Software Development Intern at <a href="https://www.honeywell.com/us/en" target ='_blank'>Honeywell Intelligrated</a>
+          </li>
+          <li>I watch a lot of movies and TV shows</li>
+          <li>I can't live without music</li>
+        </ul>
+      </div>
+      <div className='links'>
+        <a href="" target='_blank' >Github</a>
+        <a href="https://www.linkedin.com/in/amaanbilwar/" target='_blank' >LinkedIn</a>
+        <a href="https://drive.google.com/file/d/1HzOOF4F5_AMzJr99VcPgXneC5nX5_3Iy/view?usp=sharing" target='_blank'>Resume</a>
+      </div>
     </div>
   );
 }
